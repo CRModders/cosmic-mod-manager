46347395ca8e5957bc3b10f99c9504c4f686d494
Ethan <136866675+realeatham@users.noreply.github.com>
2024-04-14 00:16:04 +1000

    revert en-US.ts

--- public/locales/en/en-US.ts
+++ public/locales/en/en-US.ts
@@ -18,25 +18,6 @@ export const en_us = {
 	// By default it is set to british english content, to get away from typescript errors
 	content: {
 		...en_gb.content,
-		globals: {
-			...en_gb.content.globals
-			site: {
-				full_name: "Cosmic Reach Mod Manager",
-				short_name: "CRMM",
-			},
-			mods: "Mods",
-			resource_packs: "Resource packs",
-			modpacks: "Modpacks",
-			shaders: "Shaders",
-	},
-	home_page: {
-		...en_gb.content.home_page,
-		hero: {
-			description: {
-				line_1: "The best place for your [Cosmic Reach] mods.",
-				line_2: "Discover , play, and create content, all in one spot.",
-			},
-		},
 	},
 } satisfies {
 	meta: locale_meta;
