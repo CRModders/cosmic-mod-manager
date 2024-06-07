55cdd4feeccbcb9388211408ddc20b9a07bb37c1
abhinav <itsabhinav5383@gmail.com>
2024-06-07 12:44:37 +0530

    Reverting to use the vercel proxy

--- apps/frontend/src/hooks/fetch.ts
+++ apps/frontend/src/hooks/fetch.ts
@@ -1,6 +1,5 @@
 export default function useFetch(url: string | Request | URL, init?: FetchRequestInit | undefined) {
-	return fetch(`${process.env.VITE_SERVER_URL}${url}`, {
+	return fetch(url, {
 		...init,
-		credentials: "same-origin",
 	});
 }
