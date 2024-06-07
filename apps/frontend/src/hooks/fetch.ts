292777d168a44c5be19f39fc6a1e27d1ccc3f8d0
abhinav <itsabhinav5383@gmail.com>
2024-06-07 20:26:25 +0530

    cross subdomain cookie sharing fix

--- apps/frontend/src/hooks/fetch.ts
+++ apps/frontend/src/hooks/fetch.ts
@@ -1,5 +1,6 @@
 export default function useFetch(url: string | Request | URL, init?: FetchRequestInit | undefined) {
 	return fetch(`${process.env.VITE_SERVER_URL}${url}`, {
 		...init,
+		credentials: "include",
 	});
 }
