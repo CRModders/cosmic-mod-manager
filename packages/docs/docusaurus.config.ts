6eef2b12ab876367197d7c28d7db821fccb6cdfe
Abhinav <itsabhinav5383@gmail.com>
2024-09-26 18:53:10 +0530

    Use dark as default theme

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -86,6 +86,10 @@ const config: Config = {
             theme: prismThemes.github,
             darkTheme: prismThemes.dracula,
         },
+        colorMode: {
+            defaultMode: "dark",
+            respectPrefersColorScheme: true,
+        },
     } satisfies Preset.ThemeConfig,
 };
 
