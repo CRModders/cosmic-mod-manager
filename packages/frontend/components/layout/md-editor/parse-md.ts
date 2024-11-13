ff2c336a6afea60f231ac71e1e4430a5e2a29f1a
Abhinav <itsabhinav5383@gmail.com>
2024-11-13 07:40:29 +0530

    Fix overriding width on markdown images

--- packages/frontend/components/layout/md-editor/parse-md.ts
+++ packages/frontend/components/layout/md-editor/parse-md.ts
@@ -32,6 +32,7 @@ export const configuredXss = new FilterXSS({
     },
     onTag: (tag, html, { isClosing }) => {
         if (tag === "img" && !isClosing) {
+            console.log(html);
             return `<${tag} loading="lazy" ${html.slice(5)}`;
         }
 
