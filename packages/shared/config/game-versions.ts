eee759abe660c52f1d20b5dcc46446ec3b681135
Abhinav <itsabhinav5383@gmail.com>
2024-11-18 07:42:09 +0530

    Add new game version to list

--- packages/shared/config/game-versions.ts
+++ packages/shared/config/game-versions.ts
@@ -9,6 +9,12 @@ export interface GameVersion {
 
 const GAME_VERSIONS: GameVersion[] = [
     {
+        label: "0.3.7",
+        value: "0.3.7",
+        releaseType: GameVersionReleaseType.ALPHA,
+        major: false,
+    },
+    {
         label: "0.3.6",
         value: "0.3.6",
         releaseType: GameVersionReleaseType.ALPHA,
