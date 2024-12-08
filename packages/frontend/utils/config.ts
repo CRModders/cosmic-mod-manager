fb0ef4342925102e6ef628e1097d80ceacc773bf
Abhinav <itsabhinav5383@gmail.com>
2024-12-08 21:27:15 +0530

    :)

--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,9 +1,10 @@
 const Config = {
     FRONTEND_URL: "https://crmm.tech",
-    // BACKEND_URL: "https://api.crmm.tech",
+    BACKEND_URL: "https://api.crmm.tech",
+    BACKEND_URL_LOCAL: "http://localhost:5500",
 
     // FRONTEND_URL: "http://localhost:3000",
-    BACKEND_URL: "http://localhost:5500",
+    // BACKEND_URL: "http://localhost:5500",
 
     proxy: false,
 };
