b7f3e8c876cb2a0efe8149389d28fb612338f566
Abhinav <itsabhinav5383@gmail.com>
2024-12-17 01:22:38 +0530

    messed with bundle splitting again

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -23,46 +23,39 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                experimentalMinChunkSize: 33_000, // 5kb
                 manualChunks: (id) => {
-                    if (id.includes("css")) return "styles";
-
-                    // Routes
-                    if (id.includes("project")) return "project";
-                    if (id.includes("user")) return "user";
-                    if (id.includes("dashboard")) return "dashboard";
-                    if (id.includes("settings")) return "settings";
-                    if (id.includes("auth")) return "auth";
-                    if (id.includes("organization")) return "organization";
-                    if (id.includes("legal")) return "legal";
-                    if (id.includes("search")) return "search";
-
-                    // Catchall for leftover routes
-                    if (id.includes("routes")) return "routes";
-                    if (id.includes("pages")) return "pages";
-
-                    if (id.includes("hooks")) return "hooks";
-                    if (id.includes("components/ui")) return "ui";
-                    if (id.includes("components")) return "components";
-                    if (id.includes("utils")) return "misc";
-                    if (id.includes("/app/")) return "root";
-                    if (id.includes("schemas") || id.includes("zod")) return "zod";
-
                     // Libs
-                    if (id.includes("highlight.js")) return "highlight-js";
-                    if (id.includes("@tanstack")) return "tanstack";
-                    if (id.includes("radix") || id.includes("lucide")) return "ui-lib";
-                    if (id.includes("xss") || id.includes("cssfilter")) return "xss";
-                    if (id.includes("markdown-it") || id.includes("mdurl")) return "markdown-it";
-                    if (id.includes("react")) return "react";
-                    if (id.includes("use-")) return "lib-hooks";
+                    if (id.includes("/react")) return "react";
+                    if (id.includes("/radix") || id.includes("lucide")) return "ui-lib";
+                    if (id.includes("/markdown-it") || id.includes("mdurl")) return "markdown-it";
+                    if (id.includes("/highlight.js")) return "highlight-js";
+                    if (id.includes("/xss") || id.includes("cssfilter")) return "xss";
+                    if (id.includes("/@tanstack")) return "tanstack";
 
-                    if (id.includes("node_modules")) return "vendor";
-                    return "app";
+                    if (id.includes("shared")) return "shared";
+                    if (id.includes("types")) return "types";
+                    if (id.includes("config")) return "config";
+                    if (id.includes("icon")) return "icons";
+
+                    // Components
+                    if (id.includes("components/layout")) return "layout";
+                    if (id.includes("components/ui")) return "ui";
+                    if (id.includes("components")) return "layout";
+
+                    if (id.includes("routes")) return "routes";
+
+                    // Pages
+                    if (id.includes("user")) return "user";
+                    if (id.includes("organization/settings")) return "organization-settings";
+                    if (id.includes("organization")) return "organization";
+                    if (id.includes("settings")) return "settings";
+                    if (id.includes("search")) return "search";
+                    if (id.includes("legal")) return "legal";
+                    if (id.includes("project/settings")) return "project-settings";
+                    if (id.includes("project")) return "project";
                 },
             },
         },
-        cssCodeSplit: false,
     },
     plugins: [reactRouter(), tsconfigPaths()],
 });
