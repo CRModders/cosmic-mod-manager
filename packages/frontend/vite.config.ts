f6bdf32180138db8346cf91e3e9bd417630572eb
Abhinav <itsabhinav5383@gmail.com>
2024-12-16 22:37:49 +0530

    more fine grained module splitting

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -24,15 +24,45 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 experimentalMinChunkSize: 33_000, // 5kb
-                format: "es",
                 manualChunks: (id) => {
-                    if (id.includes("node_modules")) {
-                        return "vendor";
-                    }
+                    if (id.includes("css")) return "styles";
+
+                    // Routes
+                    if (id.includes("project")) return "project";
+                    if (id.includes("user")) return "user";
+                    if (id.includes("dashboard")) return "dashboard";
+                    if (id.includes("settings")) return "settings";
+                    if (id.includes("auth")) return "auth";
+                    if (id.includes("organization")) return "organization";
+                    if (id.includes("legal")) return "legal";
+                    if (id.includes("search")) return "search";
+
+                    // Catchall for leftover routes
+                    if (id.includes("routes")) return "routes";
+                    if (id.includes("pages")) return "pages";
+
+                    if (id.includes("hooks")) return "hooks";
+                    if (id.includes("components/ui")) return "ui";
+                    if (id.includes("components")) return "components";
+                    if (id.includes("utils")) return "misc";
+                    if (id.includes("/app/")) return "root";
+                    if (id.includes("schemas") || id.includes("zod")) return "zod";
+
+                    // Libs
+                    if (id.includes("highlight.js")) return "highlight-js";
+                    if (id.includes("@tanstack")) return "tanstack";
+                    if (id.includes("radix") || id.includes("lucide")) return "ui-lib";
+                    if (id.includes("xss") || id.includes("cssfilter")) return "xss";
+                    if (id.includes("markdown-it") || id.includes("mdurl")) return "markdown-it";
+                    if (id.includes("react")) return "react";
+                    if (id.includes("use-")) return "lib-hooks";
+
+                    if (id.includes("node_modules")) return "vendor";
                     return "app";
                 },
             },
         },
+        cssCodeSplit: false,
     },
     plugins: [reactRouter(), tsconfigPaths()],
 });
