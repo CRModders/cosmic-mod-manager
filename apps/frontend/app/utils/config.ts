4772cc4e562f56d77dad6e5f02923e8582459863
Abhinav <itsabhinav5383@gmail.com>
2024-12-18 21:56:14 +0530

    fix frontend config

--- apps/frontend/app/utils/config.ts
+++ apps/frontend/app/utils/config.ts
@@ -1,10 +1,8 @@
 const isDev = import.meta.env.DEV;
 
 let Config = {
-    FRONTEND_URL: "http://localhost:3000",
-    BACKEND_URL: "http://localhost:5500",
-    // FRONTEND_URL: "https://crmm.tech",
-    // BACKEND_URL: "https://api.crmm.tech",
+    FRONTEND_URL: "https://crmm.tech",
+    BACKEND_URL: "https://api.crmm.tech",
     BACKEND_URL_LOCAL: "http://localhost:5500",
     proxy: false,
 };
