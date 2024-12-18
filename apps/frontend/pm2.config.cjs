ea047cf90d5073c4925aa8db87e1b7c61946e105
Abhinav <itsabhinav5383@gmail.com>
2024-12-18 21:59:18 +0530

    add frontend pm2 config

---
+++ apps/frontend/pm2.config.cjs
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config({ path: "./.env" });
+
+const rootDir = "/var/www/cosmic-mod-manager";
+const frontendDir = `${rootDir}/source/packages/frontend`; // Root of the frontend
+const reloadFrontend = "pm2 reload pm2.config.cjs --only crmm-frontend";
+
+const apps = [
+    {
+        name: "crmm-frontend",
+        command: "bun run start",
+        cwd: frontendDir,
+        autorestart: true,
+        watch: false,
+    },
+];
+
+module.exports = {
+    apps: apps,
+    deploy: {
+        frontend: {
+            user: `${process.env.SSH_USER}`,
+            host: [`${process.env.SSH_HOST}`],
+            key: `${process.env.SSH_KEY}`,
+            ref: "origin/main",
+            repo: "https://github.com/CRModders/cosmic-mod-manager.git",
+            path: rootDir,
+            "post-deploy": `cd ${frontendDir} && bun install && bun run build && ${reloadFrontend}`,
+        },
+    },
+};
