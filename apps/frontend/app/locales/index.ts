828747af1dba8fac930066a411cace1c14b05c14
Abhinav <itsabhinav5383@gmail.com>
2025-01-04 19:47:56 +0530

    remove the es-ES locale that was just for testing

--- apps/frontend/app/locales/index.ts
+++ apps/frontend/app/locales/index.ts
@@ -12,10 +12,6 @@ export async function getLocaleFile(locale: string) {
     let module = null;
 
     switch (parseLocale(locale)) {
-        case "es-ES":
-            module = await import("./es-ES/translation");
-            break;
-
         case "es-419":
             module = await import("./es-419/translation");
             break;
