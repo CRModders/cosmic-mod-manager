a22cd248fbee46cb9d0fedf3cee102455f2056dd
Abhinav5383 <itsabhinav5383@gmail.com>
2025-05-14 09:27:29 +0530

    fix `ISO_DateStr` function resulting in invalid date

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -52,7 +52,7 @@ export function date(date: string | Date) {
 
 export function ISO_DateStr(date?: string | Date, utc = false): string {
     try {
-        const _date = new Date(date);
+        const _date = date ? new Date(date) : new Date();
         if (utc === false) {
             return `${_date.getFullYear()}-${(_date.getMonth() + 1).toString().padStart(2, "0")}-${_date.getDate().toString().padStart(2, "0")}`;
         }
